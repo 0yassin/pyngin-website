aa7d22898959f30d628257b8b4b1103f1251cea8
0yassin <yassinz8275@gmail.com>
2026-06-15 05:34:15 +0100

    Added board with custom pieces and validation using react-chessboard, setup the font and theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,107 @@
-import Image from "next/image";
+"use client"; 
+import { useState } from "react";
+import { Chessboard } from 'react-chessboard';
+import { Chess } from 'chess.js'; 
+
+const DARK_PIECE_COLUMNS: Record<string, number> = {
+  'bP': 0, 'bN': 1, 'bR': 2, 'bB': 3, 'bQ': 5, 'bK': 4
+};
+
+const WHITE_PIECE_COLUMNS: Record<string, number> = {
+  'wP': 0, 'wN': 1, 'wR': 2, 'wB': 3, 'wQ': 5, 'wK': 4
+};
 
 export default function Home() {
+  const [game, setGame] = useState(new Chess());
+  const [position, setPosition] = useState(game.fen());
+  const [thinking, setThinking] = useState(false);
+  const [playas, setPlayas] = useState('w');
+
+  const customPieces: Record<string, any> = {};
+
+  Object.keys(DARK_PIECE_COLUMNS).forEach((piece) => {
+    const colIndex = DARK_PIECE_COLUMNS[piece];
+    customPieces[piece] = () => (
+      <div
+        style={{
+          width: '100%',
+          aspectRatio: 1,
+          backgroundImage: `url('/pieces/d_pieces.png')`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: `600% 100%`,
+          backgroundPosition: `${colIndex/5*100}% 0px`,
+          transform: 'scale(0.75)',
+          transformOrigin:'center',
+          imageRendering: 'pixelated'
+          
+
+        }}
+      />
+    );
+  });
+
+  Object.keys(WHITE_PIECE_COLUMNS).forEach((piece) => {
+    const colIndex = WHITE_PIECE_COLUMNS[piece];
+    customPieces[piece] = () => (
+      <div
+        style={{
+          width: '100%',
+          aspectRatio: 1,
+          backgroundImage: `url('/pieces/w_pieces.png')`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: `600% 100%`,
+          backgroundPosition: `${colIndex/5*100}% 0px`,
+          transform: 'scale(0.75)',
+          transformOrigin:'center',
+          imageRendering: 'pixelated'
+        }}
+      />
+    );
+  });
+
+  interface PieceDropHandlerArgs {
+    piece: string;
+    sourceSquare: string;
+    targetSquare: string;
+  }
+
+  function onDrop({ piece, sourceSquare, targetSquare }: any) {
+    try {
+      const move = game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: "q",
+      });
+
+      if (move === null) return false;
+
+      setPosition(game.fen()); 
+      return true;
+    } catch (error) {
+      return false; 
+    }
+  }
+ const chessboardOptions = {
+    id: 'pyngin-board',
+    position: position,
+    onPieceDrop: onDrop,
+    pieces: customPieces,                              
+    darkSquareStyle: { backgroundColor: "#784F48" },   
+    lightSquareStyle: { backgroundColor: "#E2D5A1" }   
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="min-h-screen w-screen font-dogica bg-[#403241] text-white">
+      <main className="grid w-full min-h-screen h-full grid-cols-2 p-12 gap-8 items-center">
+        
+        <div className="w-full max-w-[500px] aspect-square justify-self-end shadow-2xl rounded overflow-hidden">
+          <Chessboard options={chessboardOptions} />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="w-full h-full flex flex-col justify-center p-6">
+          <h1 className="text-3xl font-dogica">Depth</h1>
         </div>
+
       </main>
     </div>
   );
